ad5656bf7403c0fdf876bb434045e79bfa21a535
persan <per.s.sandberg@bahnhof.se>
2020-02-23 16:34:17 +0100

    Remove Treace printouts

--- compiler/src/protoc-ada.cc
+++ compiler/src/protoc-ada.cc
@@ -14,7 +14,9 @@ int main(int argc, char* argv[]) {
 
   std::string invocation_name = argv[0];
   std::string invocation_basename = invocation_name.substr(invocation_name.find_last_of("/") + 1);
+
   const std::string legacy_name = "protoc-gen-ada";
+
   if (PROTOBUF_VERSION != REQIRED_PROTOBUF_VERSION ){
     std::cout << "Invalid version expected:" << REQIRED_PROTOBUF_VERSION << " got:" << PROTOBUF_VERSION << "\n";
     return -1;
@@ -22,21 +24,12 @@ int main(int argc, char* argv[]) {
 
 
   google::protobuf::compiler::ada::AdaGenerator *ada_generator = new google::protobuf::compiler::ada::AdaGenerator();
-  ofstream outfile;
-  outfile.open("Trace.txt");
-  for(int i=0; i<argc; i++) {
-    outfile << invocation_basename << endl;
-    outfile << legacy_name << endl;
-  }
 
   if (invocation_basename == legacy_name) {
     cli.RegisterGenerator("--ada_out", ada_generator, "Generate Ada specification and body.");
-    outfile << "invocation_basename == legacy_name";
-    outfile.close();
+
     return cli.Run(argc, argv);
   } else {
-    outfile << "invocation_basename != legacy_name";
-    outfile.close();
     return google::protobuf::compiler::PluginMain(argc, argv, ada_generator);
   };
 }
